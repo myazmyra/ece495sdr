4adf166fa7910b643506c41f1c41a782606e63c8
Muhammet Yazmyradov <myazmyra@purdue.edu>
2016-02-23 11:19:40 -0500

    can transmit bpsk signal with channel rate 1000

--- BPSK_tx.cpp
+++ BPSK_tx.cpp
@@ -1,5 +1,6 @@
 #include "USRP_tx.h"
 #include "BPSK_tx.h"
+#include <cstdlib>
 
 /***********************************************************************
  * Signal handlers
@@ -7,7 +8,7 @@
 static bool stop_signal_called = false;
 void sig_int_handler(int){stop_signal_called = true;}
 
-BPSK_tx::BPSK_tx() : sample_rate(12.5e6), f_c(4e6), spb(9075), bit_rate(1000),
+BPSK_tx::BPSK_tx() : sample_rate(12.5e6), f_c(4e6), bit_rate(1000), spb(12500),
                      samples_per_bit(((int) sample_rate) / bit_rate),
                      positive(samples_per_bit), negative(samples_per_bit) {
     //do something, maybe check for aliasing after you get your 3-dB frequencies from the analog guys?
@@ -38,15 +39,12 @@ size_t BPSK_tx::get_spb() {
     return spb;
 }
 
-void BPSK_tx::modulate(std::vector<int> bits, int N, std::vector< std::complex<float> > buff) {
-    int transmit_size = N * samples_per_bit;
-    for(int i = 0; i < N; i++) {
-        if(bits[i] == 1) {
-            for(int n = 0; n < transmit_size; n++) {
+int BPSK_tx::get_bit_rate() {
+    return bit_rate;
+}
 
-            }
-        }
-    }
+std::vector< std::complex<float> > BPSK_tx::modulate(int bit) {
+    return bit == 1 ? positive : negative;
 }
 
 int main(int argc, char *argv[]) {
@@ -54,17 +52,25 @@ int main(int argc, char *argv[]) {
     const double sample_rate = bpsk_tx.get_sample_rate();
     const double f_c = bpsk_tx.get_f_c();
     const size_t spb = bpsk_tx.get_spb();
-    const unsigned int bit_rate = bpsk_tx.get_bit_rate();
+    const int bit_rate = bpsk_tx.get_bit_rate();
     USRP_tx usrp_tx(sample_rate, f_c, spb);
-    const size_t samples_per_bit = ((int) sample_rate) / bit_rate;
+    //const size_t samples_per_bit = ((int) sample_rate) / bit_rate;
 
     //makes it possible to stop the program by pressing Ctrl+C
     std::signal(SIGINT, &sig_int_handler);
     std::cout << "Press Ctrl + C to stop streaming..." << std::endl << std::endl;
 
-    std::vector< std::complex<float> > buff(spb);
+    std::vector<int> bits(60 * bit_rate, 1);
+    /*for(int i = 0; i < (int) bits.size(); i++) {
+        bits[i] = std::rand() % 2;
+    }*/
+    std::vector< std::complex<float> > buff;
     usrp_tx.send_start_of_burst();
-    usrp_tx.transmit(buff, buff.size());
+    for(int i = 0; i < (int) bits.size(); i++) {
+        if(stop_signal_called) break;
+        buff = bpsk_tx.modulate(bits[i]);
+        usrp_tx.transmit(buff, buff.size());
+    }
     usrp_tx.send_end_of_burst();
 
     return EXIT_SUCCESS;
