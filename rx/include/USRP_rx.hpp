ee66b2f3cc8e2022a3d711bd1c5eb22f813add13
Muhammet Yazmyradov <myazmyra@purdue.edu>
2016-03-31 20:20:55 -0400

    remove unnecessary includes

--- rx/include/USRP_rx.hpp
+++ rx/include/USRP_rx.hpp
@@ -1,11 +1,3 @@
-#include <uhd/types/tune_request.hpp>
-#include <uhd/utils/thread_priority.hpp>
-#include <uhd/utils/safe_main.hpp>
-#include <uhd/usrp/multi_usrp.hpp>
-#include <uhd/exception.hpp>
-#include <boost/program_options.hpp>
-#include <boost/format.hpp>
-#include <boost/thread.hpp>
 #include <iostream>
 #include <fstream>
 #include <csignal>
