4512777ac1c9d7c974dc25d035e64da9d4e12401
Muhammet Yazmyradov <myazmyra@purdue.edu>
2016-03-31 20:10:18 -0400

    recompile

--- rx/include/USRP_rx.hpp
+++ rx/include/USRP_rx.hpp
@@ -20,18 +20,19 @@ class USRP_rx {
 
     USRP_rx(double sample_rate, size_t spb);
     ~USRP_rx();
-    size_t receive(std::vector< std::complex<float> > &buff) const;
+    size_t receive(std::vector< std::complex<float> > &buff);
     void issue_start_streaming();
     void issue_stop_streaming();
 
  private:
 
+    double const sample_rate; //sample_rate between the PC and Motherboard
+    size_t spb;
+
     std::string const &args; //single uhd device address args (example: "addr=192.168.10.2", "serial=901", "type=usrp2")
     std::string const &ref; //clock reference (internal, external, mimo, gpsdo)
     std::string const &cpufmt; //cpu sample format e.g. "fc32"
     std::string const &otw; //specify over the wire sample mode e.g. "sc16"
-    double const sample_rate; //sample_rate between the PC and Motherboard
-    size_t spb;
 
     uhd::usrp::multi_usrp::sptr usrp_rx; //usrp device
     uhd::stream_args_t stream_args; //stream arguments
