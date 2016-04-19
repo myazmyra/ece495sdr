d7dc6830994b6c97d31fb41aaf9d410ae83fce42
Muhammet Yazmyradov <myazmyra@purdue.edu>
2016-04-19 11:09:20 -0400

    removed pulse shaping

--- tx/src/BPSK_tx.cpp
+++ tx/src/BPSK_tx.cpp
@@ -8,18 +8,9 @@ BPSK_tx::BPSK_tx(double sample_rate,
                  spb(spb) {
 
     double T_s = 1 / sample_rate;
-    double bit_rate = sample_rate / (double) spb;
-    double beta = 0.5;
-
-    std::vector<float> pulse_shape(spb);
     for(int n = 0; n < (int) spb; n++) {
-        pulse_shape[n] = std::cos(M_PI * beta * n * T_s / bit_rate) / (1 - (2 * beta * n * T_s / bit_rate) * (2 * beta * n * T_s / bit_rate)) *
-                         std::sin(M_PI * n * T_s / bit_rate) / (M_PI * n * T_s / bit_rate);
-    }
-
-    for(int n = 0; n < (int) spb; n++) {
-        positive.push_back(std::cos(2 * M_PI * f_c * n * T_s) * pulse_shape[n]);
-        negative.push_back(-std::cos(2 * M_PI * f_c * n * T_s) * pulse_shape[n]);
+        positive.push_back(std::cos(2 * M_PI * f_c * n * T_s));
+        negative.push_back(-std::cos(2 * M_PI * f_c * n * T_s));
     }
 
 }
